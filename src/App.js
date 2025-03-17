f6b413ae5934b2b135b8702c6d524d57bbbfb554
YASHMITHA A <yashmithavagman@gmail.com>
2025-03-17 23:54:48 +0530

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/NavBar/navbar";
+import NavBar from "./components/NavBar/navbar";
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
     </div>
   );
 }
